b5a2231d57bb4e7af51a896ad5caad710b5ed19e
zmax777 <zmax777@sina.com>
2018-12-17 22:08:33 +0800

    test luyou

---
+++ src/components/Show.vue
@@ -0,0 +1,22 @@
+<template>
+  <div clss="showtime">
+    <h1>This is show pape</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Show',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Show from '@/components/Show'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Show',
+      component: Show
     }
   ]
 })
