2a2220208ea8d68e69adab4b0913e5e7ee4af9b1
zmax777 <zmax777@sina.com>
2018-12-17 21:26:55 +0800

    test babel

---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>This is an Index page</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is an login page</h1>
+  </div>
+</template>
